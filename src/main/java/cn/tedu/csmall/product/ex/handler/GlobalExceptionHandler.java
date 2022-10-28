8c6975cd7cbd269e1619a5ad5183081a24da3662
jstart <11465401+jstart@user.noreply.gitee.com>
2022-10-28 13:25:02 +0800

    关于Spring、Lombok、Knife4j框架的后端项目

--- src/main/java/cn/tedu/csmall/product/ex/handler/GlobalExceptionHandler.java
+++ src/main/java/cn/tedu/csmall/product/ex/handler/GlobalExceptionHandler.java
@@ -39,9 +39,9 @@ public class GlobalExceptionHandler {
      * @param e 全局的异常类
      * @return
      */
-//    @ExceptionHandler
-//    public String handlerServiceException(Throwable e){
-//        log.debug("这是一个Throwable异常,将统一处理");
-//        return e.getMessage();
-//    }
+    @ExceptionHandler
+    public String handlerServiceException(Throwable e){
+        log.debug("这是一个Throwable异常,将统一处理");
+        return e.getMessage();
+    }
 }
