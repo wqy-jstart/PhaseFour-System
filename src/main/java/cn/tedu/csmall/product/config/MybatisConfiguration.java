5253d2243c53f5d4eb529e756df020c00b6d362c
jstart <11465401+jstart@user.noreply.gitee.com>
2022-11-18 09:17:08 +0800

    基于Spring后端项目

--- src/main/java/cn/tedu/csmall/product/config/MybatisConfiguration.java
+++ src/main/java/cn/tedu/csmall/product/config/MybatisConfiguration.java
@@ -1,8 +1,14 @@
 package cn.tedu.csmall.product.config;
 
+import cn.tedu.csmall.product.mybatis.InsertUpdateTimeInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * MyBatis配置类
@@ -17,5 +23,19 @@ public class MybatisConfiguration {
     //测试配置是否加载成功,输出对应日志内容
     public MybatisConfiguration(){
         log.info("创建配置类对象:MybatisConfiguration");
+    }
+
+    @Autowired
+    private List<SqlSessionFactory> sqlSessionFactoryList;
+
+    // 此注解添加在方法上，表示此方法是Spring Bean的生命周期方法中的初始化方法，
+    // 会在创建对象、自动装配之后，自动执行,仅可拦截一条数据,批量时无法生效
+    @PostConstruct
+    public void addInterceptor(){
+        log.debug("开始注册Mybatis拦截器");
+        InsertUpdateTimeInterceptor interceptor = new InsertUpdateTimeInterceptor();// 创建插入修改时间的拦截器
+        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) { // 遍历sqlsession工厂集合
+            sqlSessionFactory.getConfiguration().addInterceptor(interceptor);// 将拦截器传入
+        }
     }
 }
