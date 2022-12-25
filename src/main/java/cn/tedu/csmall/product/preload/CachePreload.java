b74c2ef37a98833290ca9966850a8c3c8e07b669
wuqingyuan <11465401+jstart@user.noreply.gitee.com>
2022-12-25 18:52:55 +0800

    第四阶段Spring框架内容

--- src/main/java/cn/tedu/csmall/product/preload/CachePreload.java
+++ src/main/java/cn/tedu/csmall/product/preload/CachePreload.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @Version 0.0.1
  */
 @Slf4j
-//@Component // 声明是一个组件类,在项目启动时被加载一次
+@Component // 声明是一个组件类,在项目启动时被加载一次
 public class CachePreload implements ApplicationRunner {
 
     // 注入品牌的Mapper层接口
